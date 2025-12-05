bf5a37dd9eb52de4221364a5c0b6b41e9b65644d
pablitoguapo <pmg82260@lasallebajio.edu.mx>
2025-12-05 12:41:32 -0600

    despliegue6

--- swagger.js
+++ swagger.js
@@ -1,33 +1,33 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-// URL base dinámico (Render o local)
-const BASE_URL = process.env.BASE_URL || "http://localhost:4000";
+// Render pone su dominio en process.env.RENDER_EXTERNAL_URL
+const BASE_URL = process.env.RENDER_EXTERNAL_URL || "http://localhost:4000";
 
 const swaggerDefinition = {
-  openapi: "3.0.0",
+  openapi: '3.0.0',
   info: {
-    title: "Documentación de la API",
-    version: "1.0.0",
-    description: "Documentación de la API con Swagger",
+    title: 'Documentación de la API',
+    version: '1.0.0',
+    description: 'Documentación de la API con Swagger'
   },
   servers: [
     {
       url: BASE_URL,
-      description: "Servidor actual",
-    },
+      description: 'Servidor actual'
+    }
   ],
 };
 
 const options = {
   swaggerDefinition,
-  apis: ["./routes/*.js"],
-};
+  apis: ['./routes/*.js'],
+}
 
 const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwagger = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-};
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
 
 module.exports = setupSwagger;
